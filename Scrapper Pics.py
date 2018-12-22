e80ecd8cb31e0fb433976fef2dbcfea85b5dc8b8
gakros <atgogotsis@gmail.com>
2018-12-22 03:00:23 +0200

    Add files via upload

---
+++ Scrapper Pics.py
@@ -0,0 +1,25 @@
+#! /usr/bin/python
+import re
+import requests
+from bs4 import BeautifulSoup
+
+site = 'http://localhost/joomla/index.php/en/'
+
+response = requests.get(site)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+img_tags = soup.find_all('img')
+
+urls = [img['src'] for img in img_tags]
+
+
+for url in urls:
+    filename = re.search(r'/([\w_-]+[.](jpg|gif|png))$', url)
+    with open(filename.group(1), 'wb') as f:
+        if 'http' not in url:
+            # sometimes an image source can be relative 
+            # if it is provide the base url which also happens 
+            # to be the site variable atm. 
+            url = '{}{}'.format(site, url)
+        response = requests.get(url)
+        f.write(response.content)
